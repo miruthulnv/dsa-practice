7b7611716afa0d6f1215bea472da059ebe14ad93
Miruthul N V <137715187+miruthulnv@users.noreply.github.com>
2024-02-19 01:33:48 +0530

    Update Longest Substring Without Repeating Characters.py

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -8,15 +8,18 @@ class Solution(object):
         sub =''
         out = 0
         i=0
+        j=0
         out_L = []
         while (i<le):
             if(s[i] not in sub):
                 sub+=s[i]
                 out+=1
             else:
+                j+=1
+                i=j
                 sub=s[i]
                 out_L.append(out)
                 out=1
             i+=1
-
+        out_L.append(out)
         return max(out_L)
